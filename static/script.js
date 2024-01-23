8641fd73d6dde1877ffa63f39544a884983b71a3
CSeungJoo <seungjoo13579@gmail.com>
2024-01-23 14:02:10 +0900

    웹캠 이미지 서버로 전송하는 로직 구현 URL 차후 수정 필요

---
+++ static/script.js
@@ -0,0 +1,47 @@
+let video = document.querySelector("#cam");
+let canvas = document.querySelector('#canvas');
+let context = canvas.getContext('2d');
+const SERVER_URL = "https://{host}:{port}";
+
+const postRequest = (url, file) => {
+    const request = new XMLHttpRequest();
+    const formData = new FormData();
+    formData.append('image', file); // 이미 변환된 File 객체를 직접 추가
+    request.open("POST", url);
+    request.send(formData);
+}
+
+const constraints = {
+    video: true,  // 비디오 사용 여부
+    audio: false  // 오디오 사용 여부
+};
+
+function getVideoStream() {
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(function(stream) {
+        video.srcObject = stream; // video.srcObject 사용
+      })
+      .catch(function(err) {
+        console.error('웹캠에 접근할 수 없습니다:', err);
+      });
+}
+
+function captureFrame() {
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const imageDataUrl = canvas.toDataURL('image/png');
+    let file = dataURLtoFile(imageDataUrl, "webcamImg.png"); // .png 확장자 추가
+    postRequest(SERVER_URL, file); // File 객체를 직접 전달
+}
+
+function dataURLtoFile(dataurl, filename) {
+    let arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, {type:mime});
+}
+
+setInterval(captureFrame, 200); // 예: 5초마다 실행
+
+// HTML onClick 이벤트에서 getVideoStream 호출
