c3f6247955760f9fd01d071d819cf0726769ddc6
ParkMini <parkteammini@gmail.com>
2024-01-24 17:32:38 +0900

    사용하지 않는 페이지 제거 및 라우터 수정

--- app.py
+++ app.py
@@ -7,31 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# 이미지 분류 페이지
-@app.route('/image_classification')
-def image_classification():
-    return render_template('image_classification.html')
-
-# 객체 감지 페이지
-@app.route('/object_detection')
-def object_detection():
-    return render_template('object_detection.html')
-
-# 포즈 감지 페이지
-@app.route('/pose_detection')
-def pose_detection():
-    return render_template('pose_detection.html')
-
-# AI 선택 페이지
-@app.route('/selectAI')
-def selectAI():
-    return render_template('selectAI.html')
-
-# 스타일 변경 페이지
-@app.route('/style_change')
-def style_change():
-    return render_template('style_change.html')
-
 @app.route('/cam-test')
 def setAi():
     return render_template('camTest.html')
